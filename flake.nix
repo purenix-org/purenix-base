31c6cf7ce27b29e452030cde4e217760b001b67d
(cdep)illabout <cdep.illabout@gmail.com>
2021-10-07 10:52:33 +0900

    Add flake for easily building.

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "purescript-safe-coerce";
+
+  inputs.purenix.url = "github:purenix-org/purenix";
+  inputs.nixpkgs.follows = "purenix/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, purenix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlay = self: _: {
+          purenixShell = self.mkShell {
+            packages = [
+              self.purescript
+              purenix.defaultPackage.${system}
+              self.spago
+            ];
+          };
+        };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            overlay
+          ];
+        };
+      in
+      {
+        devShell = pkgs.purenixShell;
+      }
+    );
+}
+

39e460c93670377116982f87ae96a1601126fbc4
(cdep)illabout <cdep.illabout@gmail.com>
2021-10-17 14:04:48 +0900

    Remove Control.Monad.ST.Global because we don't have effects. Also remove monadeffect instance from Control.Monad.ST.Class

--- src/Control/Monad/ST/Class.purs
+++ src/Control/Monad/ST/Class.purs
@@ -5,13 +5,13 @@ import Prelude
 import Control.Monad.ST (ST)
 import Control.Monad.ST.Global (Global)
 import Control.Monad.ST.Global as Global
-import Effect (Effect)
+-- import Effect (Effect)
 
 class MonadST s m | m -> s where
   liftST :: ST s ~> m
 
-instance monadSTEffect :: MonadST Global Effect where
-  liftST = Global.toEffect
+-- instance monadSTEffect :: MonadST Global Effect where
+--   liftST = Global.toEffect
 
 instance monadSTST :: MonadST s (ST s) where
   liftST = identity

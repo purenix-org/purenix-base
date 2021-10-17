a43032c01015501f39f2ab4283f2e539cdd2baa2
(cdep)illabout <cdep.illabout@gmail.com>
2021-10-17 11:07:01 +0900

    Remove unneccessary comment.

--- src/Data/Unfoldable.nix
+++ src/Data/Unfoldable.nix
@@ -1,25 +1,3 @@
-# exports.unfoldrArrayImpl = function (isNothing) {
-#   return function (fromJust) {
-#     return function (fst) {
-#       return function (snd) {
-#         return function (f) {
-#           return function (b) {
-#             var result = [];
-#             var value = b;
-#             while (true) { // eslint-disable-line no-constant-condition
-#               var maybe = f(value);
-#               if (isNothing(maybe)) return result;
-#               var tuple = fromJust(maybe);
-#               result.push(fst(tuple));
-#               value = snd(tuple);
-#             }
-#           };
-#         };
-#       };
-#     };
-#   };
-# };
-
 {
   # :: forall a b
   #  . (forall x. Maybe x -> Boolean)  # isNothing

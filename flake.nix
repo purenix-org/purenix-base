b29cc5291be3bf5be2b506c57e3f7004aa95682e
Viktor Kronvall <viktor.kronvall@gmail.com>
2023-03-19 04:21:00 +0900

    purifix: add prelude and arrays

--- flake.nix
+++ flake.nix
@@ -9,16 +9,22 @@
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
+    purenix.url = "github:purenix-org/purenix";
   };
   outputs = inputs:
     inputs.flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import inputs.nixpkgs {
           inherit system;
-          overlays = [ inputs.purifix.overlay ];
+          overlays = [
+            inputs.purifix.overlay
+            inputs.purenix.overlay
+          ];
         };
         purenix-pkgs = pkgs.purifix {
           src = ./.;
+          backend = pkgs.purenix;
+          withDocs = false;
         };
       in
       {

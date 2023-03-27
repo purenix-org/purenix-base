a317bc6bd3dd7caaee5d11b39dc8cac20771b7e1
Viktor Kronvall <viktor.kronvall@gmail.com>
2023-03-28 01:54:30 +0900

    add new files introduced on remote

--- update-repos.sh
+++ update-repos.sh
@@ -4,12 +4,12 @@ for d in purescript-*; do
   fi
   echo "$d"
   pushd "$d"
-  git diff "HEAD:./src" "$d/master:src" --diff-filter=MCRTUX >"../$d-src.patch"
-  git diff "HEAD:./test" "$d/master:test" --diff-filter=MCRTUX >"../$d-test.patch"
+  git diff "HEAD:./src" "$d/master:src" --diff-filter=AMCRTUX >"../$d-src.patch"
+  git diff "HEAD:./test" "$d/master:test" --diff-filter=AMCRTUX >"../$d-test.patch"
   git diff "HEAD:./CHANGELOG.md" "$d/master:CHANGELOG.md" >"../$d-CHANGELOG.patch"
   sed -e "s|a/$d|a|" -i "../$d-CHANGELOG.patch"
   popd
-  # git apply --verbose --no-index --directory "$d/src" <"$d-src.patch"
-  # git apply --verbose --no-index --directory "$d/test" <"$d-test.patch"
+  git apply --verbose --no-index --directory "$d/src" <"$d-src.patch"
+  git apply --verbose --no-index --directory "$d/test" <"$d-test.patch"
   git apply --verbose --no-index --directory "$d" <"$d-CHANGELOG.patch"
 done
